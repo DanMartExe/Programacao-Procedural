8975c9d937b8408842551238e9642c6d1c66aaad
DanMartExe <daniel.martins1@aluno.ifsp.edu.br>
2024-02-20 01:11:21 -0300

    Lista 1 - Entrada e saida, operadores aritmeticos e operadores relacionais

---
+++ Listas/Lista1/Exercicio2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Formats.Asn1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ditite a base do triangulo: ");
+        double baseTriangulo = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ditite a altura do triangulo: ");
+        double alturaTriangulo = double.Parse(Console.ReadLine());
+
+        double areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+        bool ehMaiorQue20 = areaTriangulo > 20;
+
+        Console.WriteLine("A area do triangulo eh maior que 20? " + ehMaiorQue20);
+    }
+}
---
+++ Listas/Lista1/Exercicio3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a quantidade de moedas de um centavo: ");
+        int quantidadeDeMoedas = int.Parse(Console.ReadLine());
+
+        int real = quantidadeDeMoedas / 100;
+        int centavo = quantidadeDeMoedas % 100;
+
+        Console.WriteLine("O total em dinheiro eh: " + real + " real(is) e " + centavo + " centavo(s)");
+    }
+}
