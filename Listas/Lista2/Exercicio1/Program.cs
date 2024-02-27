8b2a6e5131f346f5ab27dd854e65910340e83fc3
DanMartExe <daniel.martins1@aluno.ifsp.edu.br>
2024-02-27 09:25:24 -0300

    Lista 2 - Operadores lógicos e estruturas de decisão

---
+++ Listas/Lista2/Exercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string loginSistema = "admin";
+        const string senhaSistema = "123senha";
+
+        Console.WriteLine("Digite seu nome de usuario: ");
+        string loginDigitado = Console.ReadLine();
+
+        Console.WriteLine("Digite sua senha: ");
+        string senhaDigitada = Console.ReadLine();
+
+        if (loginDigitado == loginSistema && senhaDigitada == senhaSistema)
+        {
+            Console.WriteLine("Login realizado com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("Credenciais invalidas");
+        }
+    }
+}
---
+++ Listas/Lista2/Exercicio2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite as quatro notas do aluno: ");
+        double nota1 = double.Parse(Console.ReadLine());
+        double nota2 = double.Parse(Console.ReadLine());
+        double nota3 = double.Parse(Console.ReadLine());        
+        double nota4 = double.Parse(Console.ReadLine());
+
+        double mediaDoAluno = (nota1 + nota2 + nota3 + nota4) / 4.0;
+
+        if (mediaDoAluno >= 5.0)
+        {
+            System.Console.WriteLine("Aluno aprovado");
+        }
+        else if (mediaDoAluno >= 3.0)
+        {
+            System.Console.WriteLine("Aluno em recuperacao");
+        }
+        else
+        {
+            System.Console.WriteLine("Aluno reprovado");
+        }
+    }
+}
