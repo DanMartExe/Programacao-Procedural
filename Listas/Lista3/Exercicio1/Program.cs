8bedeb7c212519b54910463833f228a58b05ddd3
DanMartExe <daniel.martins1@aluno.ifsp.edu.br>
2024-03-08 09:28:41 -0300

    Lista3 - Lacos de Repeticao

---
+++ Listas/Lista3/Exercicio1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int NUMERO_HABITANTES = 20;
+        double maiorSalario = 0;
+        double somatorioSalarial = 0;
+        double somatorioFilhos = 0;
+
+        for(int contador = 1; contador <= NUMERO_HABITANTES; contador++)
+        {
+            Console.WriteLine("\n" + contador + "° habitante\nDigite seu salario: ");
+            double salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade de filhos que voce possui: ");
+            int numeroDeFilhos = int.Parse(Console.ReadLine());
+
+            somatorioSalarial = somatorioSalarial + salario;
+            somatorioFilhos = somatorioFilhos + numeroDeFilhos;
+
+            if (salario > maiorSalario)
+            {
+                maiorSalario = salario;
+            }
+        }
+
+        double mediaFilhos = somatorioFilhos / NUMERO_HABITANTES;
+        double mediaSalarial = somatorioSalarial / NUMERO_HABITANTES;
+
+        Console.WriteLine("\nMedia salarial = " + Math.Round(mediaSalarial, 2) +
+        " R$\nMedia do numero de filhos por habitante = " + Math.Round(mediaFilhos, 2) +
+         "\nMaior salario = " + Math.Round(maiorSalario, 2) + " R$");
+    }
+}
---
+++ Listas/Lista3/Exercicio2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const double META_PROMOCIONAL = 150.00;
+        double somatorioValorItens = 0;
+
+        Console.WriteLine("Digite a quantidade de itens comprados: ");
+        int quantidadeItens = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite os valores dos " + quantidadeItens + " itens");
+
+        while (quantidadeItens > 0)
+        {
+            double valorItem = double.Parse(Console.ReadLine());
+
+            somatorioValorItens = somatorioValorItens + valorItem;
+            quantidadeItens--;
+        }
+
+        if (somatorioValorItens > META_PROMOCIONAL)
+        {
+            Console.WriteLine("Parabens! Voce ganhou frete gratis em sua compra de valor " +
+            somatorioValorItens + " R$");
+        }
+        else
+        {
+            Console.WriteLine("A compra nao possui frete gratis");
+        }
+    }
+}
