e13030f51cb3e420f259286dcdb5b509f907e1d6
DanMartExe <daniel.martins1@aluno.ifsp.edu.br>
2024-03-14 07:20:42 -0300

    Aulas

--- Listas/Lista3/Exercicio3/Program.cs
+++ Listas/Lista3/Exercicio3/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string USUARIO_SISTEMA = "admin";
+        const string SENHA_SISTEMA = "123senha";
+        const int TENTATIVAS_MAXIMAS = 3;
+
+        int contadorTentativas = 0;
+
+        do
+        {
+            Console.WriteLine("Nome de usuario: ");
+            string usuarioDigitado = Console.ReadLine();
+
+            Console.WriteLine("Senha: ");
+            string senhaDigitada = Console.ReadLine();
+
+            if (usuarioDigitado == USUARIO_SISTEMA && senhaDigitada == SENHA_SISTEMA)
+            {
+                Console.WriteLine("Login realizado com sucesso");
+                break;
+            }
+            contadorTentativas++;
+        }
+        while (contadorTentativas < TENTATIVAS_MAXIMAS);
+
+        if (contadorTentativas >= TENTATIVAS_MAXIMAS)
+        {
+            Console.WriteLine("sua conta foi bloqueada");
+        }
+    }
+}
--- Listas/Lista3/Exercicio3/obj/Debug/net8.0/Exercicio3.AssemblyInfo.cs
+++ Listas/Lista3/Exercicio3/obj/Debug/net8.0/Exercicio3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bedeb7c212519b54910463833f228a58b05ddd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+366d265f66238e396aa723a9c1414d9173376370")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
